4c4b2260d79c1922e1f8cefcacae34d1606bf7dd
AGNIHOTRI AJAY <mathshacker0982@gmail.com>
2026-01-26 17:33:32 +0530

    fix: Improve CLI help output for Loki [#1]

--- internal/commands/help.go
+++ internal/commands/help.go
@@ -3,12 +3,13 @@ package commands
 import "fmt"
 
 func Help() {
-	fmt.Println(`Loki - a tiny VCS
-
-Commands:
-  init
-  add <files>
-  commit -m "message"
-  status
-  log`)
+	fmt.Println(`These are common loki commands used in various situations:
+  ------------------
+  init              Create an empty loki repository or reinitialize an existing one
+  add               Add file contents to the index
+                      - <files>   Files to add to the staging area
+  commit            Record changes to the repository
+                      - -m <msg>    Commit message
+  status            Show the working tree status
+  log               Show commit logs`)
 }
